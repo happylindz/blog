1157b1660488c524785d2fad802f43e164e21da4
happylindz <lindongzhou95@gmail.com>
2017-11-08 16:22:18 +0800

    update

--- code/login/spa_login_with_session/index.js
+++ code/login/spa_login_with_session/index.js
@@ -1,6 +1,5 @@
 const koa = require('koa')
 const serve = require('koa-static')
-const views = require('koa-views')
 const koaBody = require('koa-body')
 const router = require('./router')
 
@@ -8,13 +7,14 @@ const app = new koa()
 
 app.use(koaBody());
 
-app.use(views(__dirname + '/views', {
-    extension: 'ejs'
-}))
 app.use(router.routes())
     .use(router.allowedMethods());
-app.use(serve(__dirname + '/public'))
 
-app.listen(3000, () => {
+if(process.env.NODE_ENV === 'production') {
+    app.use(serve(__dirname + '/build'))    
+}
+
+// proxy
+app.listen(9000, () => {
     console.log('listening on port 3000')
 })
